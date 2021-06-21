9a18b1d17c21140bf1d795d4cda4ca97babc776c
Kwangwoo <bnc3049@gmail.com>
2021-06-21 15:22:19 -0600

    Fixed : dataloading

--- src/screens/MyProfileMainContents.js
+++ src/screens/MyProfileMainContents.js
@@ -8,18 +8,15 @@ import Input from "../components/auth/Input";
 import { gql, useMutation, useQuery } from "@apollo/client";
 import { useForm } from "react-hook-form";
 import routes from "../routes";
-import {
-    fauser
-} from "@fortawesome/free-solid-svg-icons";
+import { fauser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState, useRef } from "react";
 import MyProfileEdit from "./MyProfileEdit";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Modal from "react-modal";
 
-
 const SEE_PROFILE_QUERY = gql`
-  query seeProfile($username: String!){
+  query seeProfile($username: String!) {
     seeProfile(username: $username) {
       id
       username
@@ -52,7 +49,7 @@ const EDIT_PROFILE_MUTATION = gql`
       phoneNumber: $phoneNumber
       password: $password
       avatar: $avatar
-    ){
+    ) {
       ok
       error
       id
@@ -65,7 +62,6 @@ const Container = styled.main`
   height: 100%;
   /* background-color: black; */
   width: 90%;
-  
 `;
 
 const MainTitle = styled.div`
@@ -78,7 +74,7 @@ const MainTitle = styled.div`
   box-sizing: border-box;
 `;
 
-const EditBtn = styled.button` 
+const EditBtn = styled.button`
   border-radius: 10px;
   background: white;
   border: 1px solid;
@@ -90,7 +86,7 @@ const EditBtn = styled.button`
   box-shadow: 0px 2px 4px gray;
   cursor: pointer;
 `;
-const SaveBtn = styled.button` 
+const SaveBtn = styled.button`
   border-radius: 10px;
   background: white;
   border: 1px solid;
@@ -109,21 +105,19 @@ const InfoSection = styled.div`
   padding: 0;
 `;
 
-const InputContainer = styled.div` 
+const InputContainer = styled.div`
   flex: 0.7;
-  display: flex;  
-
+  display: flex;
 `;
 
 const AccountInfo = styled.div`
   margin-right: 20px;
-  width:100%;
-  
+  width: 100%;
 `;
 
 const PersonalInfo = styled.div`
   margin-right: 5px;
-  width:100%;
+  width: 100%;
 `;
 
 const InfoTitle = styled.h2`
@@ -141,199 +135,179 @@ const ProfileImg = styled.div`
 `;
 
 const Avatar = styled(AccountCircleIcon)`
-  object-fit: contain;  
+  object-fit: contain;
   cursor: pointer;
   flex: 0.3;
   color: black;
   width: 100px;
-  align-items:center;
+  align-items: center;
 `;
 
-const ProfileImgEdit = styled.button`
+const ProfileImgEdit = styled.button``;
 
-`;
-
-const ConfirmEditImg = styled.button`
-
-`;
+const ConfirmEditImg = styled.button``;
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '500px',
-    height: '300px',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "500px",
+    height: "300px",
   },
 };
 
-
-function MyProfileMainContents() { 
-
+function MyProfileMainContents() {
   // const { username } = useParams();
   const { data } = useQuery(SEE_PROFILE_QUERY, {
-    variables: { username: "ckdgksdl" }, //<--- 광우한테 물어보자
-    }
-  );
+    variables: { username: "Kwangwoo2" }, //<--- 광우한테 물어보자
+  });
   // console.log(data.seeProfile.username);
   const { editProfile } = useMutation(EDIT_PROFILE_MUTATION);
 
   const [disabled, setDisabled] = useState(true);
-  const [activeEditBtn, setActiveEditBtn]=useState("Edit");
+  const [activeEditBtn, setActiveEditBtn] = useState("Edit");
   const [activeConfirmPassword, setActiveConfirmPassword] = useState(false);
   const [editMode, setEditMode] = useState(false);
 
   const handleEditClick = () => {
-
     setDisabled(!disabled);
 
-    if (activeConfirmPassword){
+    if (activeConfirmPassword) {
       setActiveConfirmPassword(false);
       setActiveEditBtn("Edit");
       alert("Your profile has been saved.");
       setEditMode(false);
+    } else {
+      setActiveConfirmPassword(true);
+      setActiveEditBtn("Save");
+      setEditMode(true);
     }
-    else {
-    setActiveConfirmPassword(true);
-    setActiveEditBtn("Save");
-    setEditMode(true);
-  }};
+  };
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleOpenModal = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const handleCloseModal = () => {
     alert("Your changed has been saved.");
     setIsModalOpen(false);
-  }
+  };
 
   return (
     <Container>
       <MainTitle>
         MY PROFILE
-        <EditBtn type='submit' onClick={handleEditClick}>{activeEditBtn}</EditBtn>
+        <EditBtn type="submit" onClick={handleEditClick}>
+          {activeEditBtn}
+        </EditBtn>
       </MainTitle>
 
-      {editMode ?
+      {editMode ? (
         <MyProfileEdit></MyProfileEdit>
-      // }
-        : (
-      <InfoSection>  
-        <InputContainer>
-          <AccountInfo>
-            <InfoTitle>Account Information</InfoTitle>
-            <InfoSubTitle>User Name</InfoSubTitle>
-            <Input
-              type="text" 
-              name="username"
-              value={data.seeProfile.username}
-              disabled={disabled}
-            />
-            <InfoSubTitle>Company Name</InfoSubTitle>
-            <Input
-              type="text"
-              name="companyName"
-              value={data.seeProfile.companyName}
-              disabled={disabled}        
-            />
-            <InfoSubTitle>Email</InfoSubTitle>
-            <Input
-              type="email"
-              name="email" 
-              value={data.seeProfile.email}
-              disabled={disabled}       
-            />
-            <InfoSubTitle>Phone Number</InfoSubTitle>      
-            <Input
-              type="number"
-              name="phoneNumber"
-              value={data.seeProfile.phoneNumber}
-              disabled={disabled}        
-            />
-            <InfoSubTitle>Password</InfoSubTitle>      
-            <Input
-              type="password"
-              name="password" 
-              value={data.seeProfile.password}
-              disabled={disabled}       
-            />
-            {activeConfirmPassword && 
-            <>
-              <InfoSubTitle>Confirm Password</InfoSubTitle>      
+      ) : (
+        // }
+        <InfoSection>
+          <InputContainer>
+            <AccountInfo>
+              <InfoTitle>Account Information</InfoTitle>
+              <InfoSubTitle>User Name</InfoSubTitle>
               <Input
                 type="text"
-                name="password"           
+                name="username"
+                value={data?.seeProfile?.username}
+                disabled={disabled}
               />
-            </>
-            }      
-          </AccountInfo>
-          
-          <PersonalInfo>
-            <InfoTitle>Personal Information</InfoTitle>
-            <InfoSubTitle>Date of Birth</InfoSubTitle>
-            <Input 
-              type="text"
-              name="birth" 
-              value={data.seeProfile.birth} 
-              disabled={disabled}      
-            />
-            <InfoSubTitle>Country/Region</InfoSubTitle>
-            <Input
-              type="text"
-              name="country"
-              value={data.seeProfile.country}
-              disabled={disabled}        
-            />
-            <InfoSubTitle>State</InfoSubTitle>
-            <Input
-              type="text"
-              name="state" 
-              value={data.seeProfile.state}
-              disabled={disabled}       
-            />
-            <InfoSubTitle>City</InfoSubTitle>
-            <Input
-              type="text"
-              name="city" 
-              value={data.seeProfile.city}
-              disabled={disabled}       
-            />
-            <InfoSubTitle>Street</InfoSubTitle>      
-            <Input
-              type="text"
-              name="Street"  
-              value={data.seeProfile.Street}
-              disabled={disabled}      
-            />
-          </PersonalInfo>
-        </InputContainer> 
-        
-        <ProfileImg>
-          <Avatar>
-            {/* <AccountCircleIcon></AccountCircleIcon> */}
-          </Avatar>
-          <ProfileImgEdit onClick={handleOpenModal}>
-            Edit
-          </ProfileImgEdit>
-          <Modal 
-            isOpen={isModalOpen}
-            style={customStyles}
-          >
-            Edit Profile img
-            <ConfirmEditImg onClick={handleCloseModal}>
-              Save
-            </ConfirmEditImg>
-          </Modal>
-        </ProfileImg>
-        
-      </InfoSection>
-        )}
-      
+              <InfoSubTitle>Company Name</InfoSubTitle>
+              <Input
+                type="text"
+                name="companyName"
+                value={data?.seeProfile?.companyName}
+                disabled={disabled}
+              />
+              <InfoSubTitle>Email</InfoSubTitle>
+              <Input
+                type="email"
+                name="email"
+                value={data?.seeProfile?.email}
+                disabled={disabled}
+              />
+              <InfoSubTitle>Phone Number</InfoSubTitle>
+              <Input
+                type="number"
+                name="phoneNumber"
+                value={data?.seeProfile?.phoneNumber}
+                disabled={disabled}
+              />
+              <InfoSubTitle>Password</InfoSubTitle>
+              <Input
+                type="password"
+                name="password"
+                value={data?.seeProfile?.password}
+                disabled={disabled}
+              />
+              {activeConfirmPassword && (
+                <>
+                  <InfoSubTitle>Confirm Password</InfoSubTitle>
+                  <Input type="text" name="password" />
+                </>
+              )}
+            </AccountInfo>
+
+            <PersonalInfo>
+              <InfoTitle>Personal Information</InfoTitle>
+              <InfoSubTitle>Date of Birth</InfoSubTitle>
+              <Input
+                type="text"
+                name="birth"
+                value={data?.seeProfile?.birth}
+                disabled={disabled}
+              />
+              <InfoSubTitle>Country/Region</InfoSubTitle>
+              <Input
+                type="text"
+                name="country"
+                value={data?.seeProfile?.country}
+                disabled={disabled}
+              />
+              <InfoSubTitle>State</InfoSubTitle>
+              <Input
+                type="text"
+                name="state"
+                value={data?.seeProfile?.state}
+                disabled={disabled}
+              />
+              <InfoSubTitle>City</InfoSubTitle>
+              <Input
+                type="text"
+                name="city"
+                value={data?.seeProfile?.city}
+                disabled={disabled}
+              />
+              <InfoSubTitle>Street</InfoSubTitle>
+              <Input
+                type="text"
+                name="Street"
+                value={data?.seeProfile?.Street}
+                disabled={disabled}
+              />
+            </PersonalInfo>
+          </InputContainer>
+
+          <ProfileImg>
+            <Avatar>{/* <AccountCircleIcon></AccountCircleIcon> */}</Avatar>
+            <ProfileImgEdit onClick={handleOpenModal}>Edit</ProfileImgEdit>
+            <Modal isOpen={isModalOpen} style={customStyles}>
+              Edit Profile img
+              <ConfirmEditImg onClick={handleCloseModal}>Save</ConfirmEditImg>
+            </Modal>
+          </ProfileImg>
+        </InfoSection>
+      )}
     </Container>
   );
 }
